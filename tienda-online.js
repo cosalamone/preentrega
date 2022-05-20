ae5f6608ea370f7432db9803be5ad46b49b6bc8c
cosalamone <cosalamone@gmail.com>
2022-05-19 21:17:54 -0300

    guia de como utilizar cuentas tester y sus datos + datos tj

--- tienda-online.js
+++ tienda-online.js
@@ -76,6 +76,7 @@ window.onload = function () {
                 title: 'Compra realizada!',
                 text: 'Su compra se realizó con éxito. En los proximos 2 días hábiles podrá retirar su producto en Av. Siempreviva 742',
                 icon: 'success',
+                confirmButtonColor: '#b5df93',
                 confirmButtonText: 'Regresar a Sucu.home'
             })
         } else
